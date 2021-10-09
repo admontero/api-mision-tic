962cdca518f141631bdddcd97eb8d3748646f34d
admontero <andresdavidmonterofernandez@gmail.com>
2021-10-08 23:14:03 -0500

    Configuración inicial de la API

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: '.env' });
+const chalk = require('chalk');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+//Puerto de la app
+const PORT = process.env.PORT
+
+//post, put, delete, patch
+app.get('/', (req, res) => {
+    res.send('Hola mundo desde endpoint');
+});
+
+app.get('/productos', (req, res) => {
+    res.send('Aquí van todos los productos');
+});
+
+app.get('/usuarios', (req, res) => {
+    res.send('Aquí van todos los usuarios');
+});
+
+app.get('/ventas', (req, res) => {
+    res.send('Aquí van todas las ventas');
+});
+
+//Ruta con parámetros
+app.get('/hola/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hola ${name}`);
+});
+
+//Toma cualquier ruta no definida
+app.get('*', (req, res) => {
+    res.status(404).send('404 | Página no encontrada');
+});
+
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
